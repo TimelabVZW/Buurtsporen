21acaee49e452e8edeb22dde79f59aaa3d3e66cc
De Boeck Stef <91065663+pgm-stefdebo3@users.noreply.github.com>
2024-07-15 10:57:39 +0200

    Added imageUrl to update dto

--- backend/src/story/dto/update-story-with-blocks.input.ts
+++ backend/src/story/dto/update-story-with-blocks.input.ts
@@ -63,6 +63,9 @@ export class UpdateStoryWithBlocksInput {
     author?: string;
 
     @Field({nullable: true})
+    imageUrl?: string;
+
+    @Field({nullable: true})
     description?: string;
 
     @Field({nullable: true})
